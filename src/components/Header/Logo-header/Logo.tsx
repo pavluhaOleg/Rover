6559cf744012a22984bff2d1603b36e6fbec7b6e
pavl <githab38@gmail.com>
2022-10-08 16:22:24 +0300

    изменение в логотипе

--- src/components/Header/Logo-header/Logo.tsx
+++ src/components/Header/Logo-header/Logo.tsx
@@ -4,11 +4,9 @@ import l from './Logo.module.css'
 
 const Logo = () => {
   return (
-    <div className={l.blokLogo}>
-      <Link to='/'>
-        <img src="https://dxnav.landrover.com/current/images/logo-landrover-7bd33da9d247934b2b4929d399ab8a42.png" />
-      </Link>
-    </div>
+    <Link className={l.blokLogo} to='/'>
+      <img src="https://dxnav.landrover.com/current/images/logo-landrover-7bd33da9d247934b2b4929d399ab8a42.png" title="на главную" />
+    </Link>
   )
 }
 
